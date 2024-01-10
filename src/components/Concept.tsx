ac8de09cf9a8417406bee6dd7b23ef75d0012106
Luciano <luciano.galvao@tikiweb.com.br>
2024-01-09 21:18:09 -0300

    Correçoes componentes

--- src/components/Concept.tsx
+++ src/components/Concept.tsx
@@ -8,8 +8,14 @@ export default function Concept() {
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
 					laboriosam rerum repellendus enim rem quam similique ad distinctio eum
 					illum aliquid, accusantium fugiat quis unde ducimus vero reprehenderit
-					quo consequatur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nostrum, non modi iusto fugit corporis ullam id dolorum laudantium vitae unde nobis ab doloremque animi assumenda soluta dignissimos quo ipsam!
+					quo consequatur. Lorem ipsum dolor sit amet consectetur adipisicing
+					elit. Officiis nostrum, non modi iusto fugit corporis ullam id dolorum
+					laudantium vitae unde nobis ab doloremque animi assumenda soluta
+					dignissimos quo ipsam!
 				</p>
+				<a href="#">
+					Fale com um advogado agora <span className="icon-phone"></span>
+				</a>
 			</div>
 		</div>
 	);
